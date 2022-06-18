8534c14ffbcb64aef7003322d0b3ec0383d064a7
Ezzar616 <103579773+Ezzar616@users.noreply.github.com>
2022-06-18 17:48:04 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,13 +1,13 @@
-const PhoneNumber = require('awesome-phonenumber')
+const PhoneNumber = require('6287853163532')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;Lui gmntng;;;'
-                    + 'FN:Lui gmntng\n' // full name
+                    + 'N:;Ezzar;;;'
+                    + 'FN:Aliezzar Wijaya\n' // full name
                     + 'ORG:kalo mau masukin bot ini ke gc lu chat;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6282146092695:+62 821-4609-2695\n' // WhatsApp ID + phone number
+                    + 'TEL;type=CELL;type=VOICE;waid=6287853163532:+62 878-5316-3532\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'Lui gmntng', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Ezzar', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
